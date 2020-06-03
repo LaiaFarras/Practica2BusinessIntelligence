2497add75237f717dc85b24ea166acb464bef081
josepfontm <josep5font@gmail.com>
2020-06-03 14:37:16 +0200

    canvi

--- Test.R
+++ Test.R
@@ -18,5 +18,22 @@ if(!require("tidyverse")) {
 }
 
 df_taxis = read.csv(file="2019_Yellow_Taxi_Trip_Data.csv", nrows=300000)
-
 str(df_taxis)
+
+#
+
+sum(is.na(df_taxis$passenger_count))
+sum(is.na(df_taxis$passenger_count))
+sum(is.na(df_taxis$trip_distance))
+
+
+df_taxis=na.omit(df_taxis)
+sum(is.na(df_taxis$passenger_count))
+sum(is.na(df_taxis$trip_distance))
+
+date_start = as.numeric(df_taxis$tpep_pickup_datetime)
+
+
+
+hist(x=date_start)
+
